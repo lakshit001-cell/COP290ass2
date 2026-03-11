e26b2f83a7931e012d27c080b79044c0f23a27e8
Lakshit <lakshitsolanki001@gmail.com>
2026-03-11 06:15:28 +0000

    day5

--- src/pages/ProjectDashPage.tsx
+++ src/pages/ProjectDashPage.tsx
@@ -3,15 +3,44 @@ import { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import styles from '../styles/ProjectDash.module.css';
 import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function ProjectDash (){
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const { id } = useParams();
+    const currentUser = JSON.parse(localStorage.getItem("user") || "{}");
+
+
+
+
+
 
 
     return(
-        <h1 className={styles.text}>
-            in dev
+        <div className={styles.layout}>
+            <main className={styles.boards}>
+                <h1>
+                    kanban boards
+                </h1>
+
+            </main>
+
+           
+
+            <aside className={styles.sidebar}>
+
+                <h1> have</h1>
+
+
+
+            </aside>
+
+
+
+
+        </div>
+
         
-        </h1>
     )
 }
 
